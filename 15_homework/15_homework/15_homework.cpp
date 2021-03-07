b3ac96db422513929f618a21057ebf47bd575203
kripa59 <kripa59@yandex.ru>
2021-03-07 14:27:01 +0300

    15_4 homework

---
+++ 15_homework/15_homework/15_homework.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int x;
+	int y;
+	std::cout << "Enter number: ";
+	std::cin >> x;
+	
+	for (int y=0; y < x+1; y++)
+	{
+		if (y % 2 == 0)
+			std::cout << y << " ";
+	}
+}
+
+
+
+
+
