bcf9b345b19bdab2a2e60bd507f95cca7e1a933c
kripa59 <kripa59@yandex.ru>
2021-03-07 23:04:22 +0300

    15_4 homework double 2

--- 15_homework/15_homework/15_homework.cpp
+++ 15_homework/15_homework/15_homework.cpp
@@ -8,9 +8,16 @@ int main()
 	std::cout << "Enter number: ";
 	std::cin >> x;
 	
+	std::cout << "\n het: ";
 	for (int y=0; y < x+1; y++)
 	{
 		if (y % 2 == 0)
+			std::cout << y << " ";
+	}
+	std::cout << "\n Nehet: ";
+	for (int y = 0; y < x; y++)
+	{
+		if (y % 2 != 0)
 			std::cout << y << " ";
 	}
 }
