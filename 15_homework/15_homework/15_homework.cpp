a4cddd3cbd1070797f90ca948010908747d79d3a
kripa59 <kripa59@yandex.ru>
2021-03-08 10:23:49 +0300

    15_4 homework double 3

--- 15_homework/15_homework/15_homework.cpp
+++ 15_homework/15_homework/15_homework.cpp
@@ -1,25 +1,14 @@
 ﻿#include <iostream>
 #include <cmath>
+#include "Helpers.h"
+
+int x;
+int y;
 
 int main()
 {
-	int x;
-	int y;
-	std::cout << "Enter number: ";
-	std::cin >> x;
-	
-	std::cout << "\n het: ";
-	for (int y=0; y < x+1; y++)
-	{
-		if (y % 2 == 0)
-			std::cout << y << " ";
-	}
-	std::cout << "\n Nehet: ";
-	for (int y = 0; y < x; y++)
-	{
-		if (y % 2 != 0)
-			std::cout << y << " ";
-	}
+	func(x, y);
+
 }
 
 
---
+++ 15_homework/15_homework/Helpers.h
@@ -0,0 +1,21 @@
+#pragma once
+void func(int x, int y)
+{
+	std::cout << "Enter number: ";
+	std::cin >> x;
+
+
+	std::cout << "\n het: ";
+	for (int y = 0; y < x + 1; y++)
+	{
+		if (y % 2 == 0)
+			std::cout << y << " ";
+	}
+
+	std::cout << "\n Nehet: ";
+	for (int y = 0; y < x; y++)
+	{
+		if (y % 2 != 0)
+			std::cout << y << " ";
+	}
+}
