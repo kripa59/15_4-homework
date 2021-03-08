7d083f2368aa02bf287e27f63d9f86887897eeac
kripa59 <kripa59@yandex.ru>
2021-03-08 13:26:59 +0300

    15_4 homework double 4

--- 15_homework/15_homework/15_homework.cpp
+++ 15_homework/15_homework/15_homework.cpp
@@ -2,13 +2,35 @@
 #include <cmath>
 #include "Helpers.h"
 
-int x;
-int y;
+//int x;
+//int y;
+
+//int main()
+//{
+//	func(x, y);
+//
+//}
+
+int N;
+void func2(int N, int y)
+{
+	for (int i = y; i <= N; i += 2)
+		std::cout << i << " ";
+	std::cout << "\n";
+}
 
 int main()
 {
-	func(x, y);
+	std::cout << "Enter number: ";
+	std::cin >> N;
 
+	std::cout << "Het:\n";
+	func2(N, 0);
+
+	std::cout << "Nohet:\n";
+	func2(N, 1);
+
+	return 0;
 }
 
 
